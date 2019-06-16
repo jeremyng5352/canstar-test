faaf808c60b96bc57f87175dca269793dd5e69b3
Jeremy Yung Hau Ng <jeremy.ng1002@gmail.com>
2019-06-16 14:26:03 +1000

    setup base project

---
+++ src/app/data/restaurant.ts
@@ -0,0 +1,113 @@
+export const restaurants = [{
+        id: 1,
+        name: 'Mission Chinese Food',
+        neighborhood: 'Manhattan',
+        photograph: '1.jpg',
+        address: '171 E Broadway, New York, NY 10002',
+        latlng: {
+            lat: 40.713829,
+            lng: -73.989667
+        },
+        cuisine_type: 'Asian'
+    }, {
+        id: 2,
+        name: 'Emily',
+        neighborhood: 'Brooklyn',
+        photograph: '2.jpg',
+        address: '919 Fulton St, Brooklyn, NY 11238',
+        latlng: {
+            lat: 40.683555,
+            lng: -73.966393
+        },
+        cuisine_type: 'Pizza'
+    }, {
+        id: 3,
+        name: 'Kang Ho Dong Baekjeong',
+        neighborhood: 'Manhattan',
+        photograph: '3.jpg',
+        address: '1 E 32nd St, New York, NY 10016',
+        latlng: {
+            lat: 40.747143,
+            lng: -73.985414
+        },
+        cuisine_type: 'Asian'
+    }, {
+        id: 4,
+        name: `Katz's Delicatessen`,
+        neighborhood: 'Manhattan',
+        photograph: '4.jpg',
+        address: '205 E Houston St, New York, NY 10002',
+        latlng: {
+            lat: 40.722216,
+            lng: -73.987501
+        },
+        cuisine_type: 'American'
+    }, {
+        id: 5,
+        name: `Roberta's Pizza`,
+        neighborhood: 'Brooklyn',
+        photograph: '5.jpg',
+        address: '261 Moore St, Brooklyn, NY 11206',
+        latlng: {
+            lat: 40.705089,
+            lng: -73.933585
+        },
+        cuisine_type: 'Pizza'
+    }, {
+        id: 6,
+        name: 'Hometown BBQ',
+        neighborhood: 'Brooklyn',
+        photograph: '6.jpg',
+        address: '454 Van Brunt St, Brooklyn, NY 11231',
+        latlng: {
+            lat: 40.674925,
+            lng: -74.016162
+        },
+        cuisine_type: 'American'
+    }, {
+        id: 7,
+        name: 'Superiority Burger',
+        neighborhood: 'Manhattan',
+        photograph: '7.jpg',
+        address: '430 E 9th St, New York, NY 10009',
+        latlng: {
+            lat: 40.727397,
+            lng: -73.983645
+        },
+        cuisine_type: 'American'
+    }, {
+        id: 8,
+        name: 'The Dutch',
+        neighborhood: 'Manhattan',
+        photograph: '8.jpg',
+        address: '131 Sullivan St, New York, NY 10012',
+        latlng: {
+            lat: 40.726584,
+            lng: -74.002082
+        },
+        cuisine_type: 'American'
+    }, {
+        id: 9,
+        name: 'Mu Ramen',
+        neighborhood: 'Queens',
+        photograph: '9.jpg',
+        address: '1209 Jackson Ave, Queens, NY 11101',
+        latlng: {
+            lat: 40.743797,
+            lng: -73.950652
+        },
+        cuisine_type: 'Asian'
+    }, {
+        id: 10,
+        name: 'Casa Enrique',
+        neighborhood: 'Queens',
+        photograph: '10.jpg',
+        address: '5-48 49th Ave, Queens, NY 11101',
+        latlng: {
+            lat: 40.743394,
+            lng: -73.954235
+        },
+        cuisine_type: 'Mexican'
+    }];
+
+
