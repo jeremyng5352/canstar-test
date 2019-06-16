cdbb4d1ea64487f87d0e760a5274b7303ff111a8
Jeremy Yung Hau Ng <jeremy.ng1002@gmail.com>
2019-06-16 18:54:26 +1000

    add disclaimer banner

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { rawRestaurantDatas } from '../../data/restaurant';
 import { RestaurantService } from '../../state/restaurant.service';
 import { Restaurant } from 'src/app/state';
 import { Observable } from 'rxjs';
@@ -11,6 +10,8 @@ import { RestaurantQuery } from '../../state/restaurant.query';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  isBannerShown: boolean;
+  currentFilter: 'Asian' | 'American' | 'Pizza' | 'Mexican' | 'None';
   currentView: 'Table' | 'Grid';
   selectedCuisineType: string;
   restaurants$: Observable<Array<Restaurant>>;
@@ -19,12 +20,24 @@ export class DashboardComponent implements OnInit {
     private restaurantService: RestaurantService,
     private restaurantQuery: RestaurantQuery
   ) {
-    this.currentView = 'Grid';
+    this.isBannerShown = true;
+    this.currentView = 'Table';
+    this.currentFilter = 'None';
   }
 
   ngOnInit() {
     this.restaurantService.getRestaurants();
-    this.restaurants$ = this.restaurantQuery.selectAll();
+    this.getRestaurants();
+  }
+
+  getRestaurants() {
+    if (this.currentFilter === 'None') {
+      this.restaurants$ = this.restaurantQuery.selectAll();
+    } else {
+      this.restaurants$ = this.restaurantQuery.selectAll({
+        filterBy: entity => entity.cuisine_type === this.currentFilter
+      });
+    }
   }
 
 }
